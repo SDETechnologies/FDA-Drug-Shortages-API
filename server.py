8eef7ed3c468b62e0583383d26defb66a3ca1d58
DrugLog <androiddruglog@gmail.com>
2023-06-07 07:01:20 -0400

    Initial commit

---
+++ server.py
@@ -0,0 +1,7 @@
+import flask
+import requests
+import json
+from flask import Flask
+from flask_restful import Resource, Api, reqparse
+from flask import request
+from flask_cors import CORS, cross_origin
